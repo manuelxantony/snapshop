f17747bc464235f00ad1fa048e44e6e4cfec52d0
manuelxantony <manuelxantony@gmail.com>
2024-02-06 20:52:34 +0530

    configured postgress with versel and created seed for initial data.

---
+++ app/lib/placeholder-data.js
@@ -0,0 +1,106 @@
+const description =
+  "a state-of-the-art bike combining innovation, performance, and style. With its lightweight carbon fiber frame and advanced suspension system, it effortlessly conquers any terrain. Precision-shifting gears and high-performance disc brakes ensure unparalleled control and responsiveness. Its sleek design and vibrant colors make a bold statement wherever you ride. Whether you're a seasoned cyclist seeking adventure or a casual rider exploring new paths, the Velocity Vortex is your ultimate companion. Experience the thrill of the open road like never before";
+
+const products = [
+  {
+    id: "3958dc9e-712f-4377-85e9-fec4b6a6442a",
+    name: "blue bike",
+    price: 299,
+    description: description,
+    image_url: "/products/blue.png",
+  },
+  {
+    id: "3958dc9e-742f-4377-85e9-fec4b6a6442a",
+    name: "bulls bike",
+    price: 399,
+    description: description,
+    image_url: "/products/bulls.png",
+  },
+
+  {
+    id: "3958dc9e-737f-4377-85e9-fec4b6a6442a",
+    name: "mrx bike",
+    price: 259,
+    description: description,
+    image_url: "/products/mrx.png",
+  },
+
+  {
+    id: "50ca3e18-62cd-11ee-8c99-0242ac120002",
+    name: "polygon bike",
+    price: 499,
+    description: description,
+    image_url: "/products/polygon.png",
+  },
+
+  {
+    id: "3958dc9e-787f-4377-85e9-fec4b6a6442a",
+    name: "scott bike",
+    price: 299,
+    description: description,
+    image_url: "/products/scott.png",
+  },
+
+  {
+    id: "76d65c26-f784-44a2-ac19-586678f7c2f2",
+    name: "trek bike",
+    price: 399,
+    description: description,
+    image_url: "/products/trek.png",
+  },
+];
+
+// as of now we are setting all products as hot selling
+const hotSellingProducts = [
+  {
+    id: "3958dc9e-712f-4377-85e9-fec4b6a6442a",
+    name: "blue bike",
+    price: 299,
+    description: description,
+    image_url: "/products/blue.png",
+  },
+  {
+    id: "3958dc9e-742f-4377-85e9-fec4b6a6442a",
+    name: "bulls bike",
+    price: 399,
+    description: description,
+    image_url: "/products/bulls.png",
+  },
+
+  {
+    id: "3958dc9e-737f-4377-85e9-fec4b6a6442a",
+    name: "mrx bike",
+    price: 259,
+    description: description,
+    image_url: "/products/mrx.png",
+  },
+
+  {
+    id: "50ca3e18-62cd-11ee-8c99-0242ac120002",
+    name: "polygon bike",
+    price: 499,
+    description: description,
+    image_url: "/products/polygon.png",
+  },
+
+  {
+    id: "3958dc9e-787f-4377-85e9-fec4b6a6442a",
+    name: "scott bike",
+    price: 299,
+    description: description,
+    image_url: "/products/scott.png",
+  },
+
+  {
+    id: "76d65c26-f784-44a2-ac19-586678f7c2f2",
+    name: "trek bike",
+    price: 399,
+    description: description,
+    image_url: "/products/trek.png",
+  },
+];
+
+module.exports = {
+  hotSellingProducts,
+  products,
+};
---
+++ scripts/seed.js
@@ -0,0 +1,91 @@
+const { db } = require("@vercel/postgres");
+
+const {
+  hotSellingProducts,
+  products,
+} = require("../app/lib/placeholder-data.js");
+
+async function seedHotSellingProducts(client) {
+  try {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+    // create hotselling product table if not exists
+    const createTable = await client.sql`
+    CREATE TABLE IF NOT EXISTS hotsellingproducts (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        name VARCHAR(255) NOT NUll,
+        price INT NOT NULL,
+        description TEXT NOT NULL,
+        image_url VARCHAR(255) NOT NULL
+
+    );`;
+
+    console.log("Created hotsellingproducts table.");
+
+    const insertHotSellingProducts = await Promise.all(
+      hotSellingProducts.map((hotSellingProduct) => {
+        return client.sql`
+        INSERT INTO hotsellingproducts (id, name, price , description, image_url)
+        VALUES (${hotSellingProduct.id}, ${hotSellingProduct.name}, ${hotSellingProduct.price}, ${hotSellingProduct.description}, ${hotSellingProduct.image_url})
+        ON CONFLICT (id) DO NOTHING;
+        `;
+      })
+    );
+
+    console.log(
+      `Seeded ${insertHotSellingProducts.length} hot selling products.`
+    );
+
+    return { createTable, hotSellingProducts: insertHotSellingProducts };
+  } catch (error) {
+    console.log("Error seeding hot selling items", error);
+    throw error;
+  }
+}
+
+async function seedProducts(client) {
+  try {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+    // create products table if not exists
+    const createTable = await client.sql`
+    CREATE TABLE IF NOT EXISTS products (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        name VARCHAR(255) NOT NUll,
+        price INT NOT NULL,
+        description TEXT NOT NULL,
+        image_url VARCHAR(255) NOT NULL
+
+    );`;
+
+    console.log("Created products table.");
+
+    const insertProducts = await Promise.all(
+      products.map((product) => {
+        return client.sql`
+        INSERT INTO products (id, name, price , description, image_url)
+        VALUES (${product.id}, ${product.name}, ${product.price}, ${product.description}, ${product.image_url})
+        ON CONFLICT (id) DO NOTHING;
+        `;
+      })
+    );
+
+    console.log(`Seeded ${insertProducts.length} all products.`);
+    return { createTable, products: insertProducts };
+  } catch (error) {
+    console.log("Error seeding all products", error);
+    throw error;
+  }
+}
+
+async function main() {
+  const client = await db.connect();
+
+  // call seed methods
+  await seedHotSellingProducts(client);
+  await seedProducts(client);
+
+  await client.end();
+}
+
+main().catch((err) => {
+  console.error("An error occured while attempting to seed the databse:", err);
+});
